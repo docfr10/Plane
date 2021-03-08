4da783c0ef00d6cdeb13057ac393b98d97489b97
Nikita <docfr10@yandex.ru>
2021-03-08 15:44:48 +0300

    Fixed scripts and collider

--- Assets/Scripts/Plane.cs
+++ Assets/Scripts/Plane.cs
@@ -1,21 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Plane : MonoBehaviour
 {
+    public GameObject Player;
+    public GameObject Platform;
+    public float Speed = 10.0f;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
-    public GameObject Player;
-    public GameObject Platform;
-    public int Speed = 8;
-
-    Rigidbody rb;
-
     void Update()
     {
         if (Input.GetKey(KeyCode.Space))
